6d9780c9b2e9622605d35ef86bfd27704faa5210
Chandral Thakor <chandral.thakor@gmail.com>
2019-12-28 18:55:06 +0530

    Added Sentdex files~

--- Socket Programming/Sentdex/client.py
+++ Socket Programming/Sentdex/client.py
@@ -1,6 +1,23 @@
 import socket
 
+HEADER_SIZE = 10
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
-msg = s.recv(8)
-print(msg.decode("UTF-8"))
+s.connect((socket.gethostname(), 1236))
+
+while True:
+    full_msg = ""
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"New message length: {msg[:HEADER_SIZE]}")
+            msg_length = int(msg[:HEADER_SIZE])
+            new_msg = False
+        decoded_msg = msg.decode("utf-8")
+        full_msg += decoded_msg
+        if len(full_msg)-HEADER_SIZE == msg_length:
+            print("Full message received.")
+            print(full_msg[HEADER_SIZE:])
+            new_msg = True
+            full_msg = ""
+    print(full_msg)
