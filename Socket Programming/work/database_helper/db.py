bd79955efd361a98e9ee702ad660c733317afc9b
Chandral Thakor <chandral.thakor@gmail.com>
2019-12-31 11:39:12 +0530

    Added logging

--- Socket Programming/work/database_helper/db.py
+++ Socket Programming/work/database_helper/db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-# Configuration data
+# Database connection object
 mysql_db = mysql.connector.connect(
   host="localhost",
   user="test",
@@ -10,25 +10,37 @@ mysql_db = mysql.connector.connect(
 
 
 def connection():
+    """
+    Simply returns a cursor object of the database connection
+    :return: MYSQL DB cursor
+    """
     return mysql_db.cursor()
 
 
 def enter_data_in_db(data):
-    fields = ""
-    values = ""
+    """
+        Takes in a dictionary as a parameter, extracts dictionary keys as DB column names and the values as column's data.
+        Saves the data in the database and returns
+        :param data: Dictionary object which is the data received from the client
+        :return: A tuple with the two values 'True' and the data received if data saved successfully or a tuple with
+        one more additional value, string of Exception occurred if data submission failed.
+    """
+    fields = ""  # Empty string for fields to be used in SQL query
+    values = ""  # Empty string for field values to be used in SQL query
 
+    # Loop iterates over each key in the dictionary, modifies it as the pre-defined column name in DB and appends it to
+    # the local variable 'fields'. It also appends the value of the dictionary key to the local variable 'values'
     for key in data:
-        if '.' in key:
-            fields += key.replace('.', '_') + ', '
-        else:
-            fields += key + ", "
+        fields += str(key) + ', '
         values += str(data[key]) + ', '
 
-    fields = fields.strip()[:-1]
-    values = values.strip()[:-1]
-    print(fields)
-    print(values)
-    input()
-    connection().execute("INSERT INTO clientData ({}) VALUES ({})".format(fields, values))
-    mysql_db.commit()
+    fields = fields.replace('.', '_')  # Replaces '.' with '_' as per the pre-defined column names in the database
+    fields = fields.strip()[:-1]  # Removes the extra ',' appended to the string on the last iteration
+    values = values.strip()[:-1]  # Removes the extra ',' appended to the string on the last iteration
 
+    try:
+        connection().execute("INSERT INTO clientData ({}) VALUES ({})".format(fields, values))  # SQL query to insert
+        mysql_db.commit()  # Committing changes to the DB
+        return True, fields + ' | ' + values
+    except Exception as E:
+        return False, fields + ' | ' + values, str(E)
--- Socket Programming/work/server.py
+++ Socket Programming/work/server.py
@@ -6,10 +6,19 @@ from database_helper import db
 
 IP = "127.0.0.1"
 PORT = 2021
+LOG = open("Logs/socket_server.log", "a")
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allows to use the same IP/Port on server restart
 s.bind((IP, PORT))
 s.listen(5)
+
+
+def log_entry(msg):
+    timestamp = "[{}] ".format(datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S"))
+    print(timestamp + msg)
+    LOG.write(timestamp + msg + "\n")
 
 
 def _change_data_to_dict(data):
@@ -27,10 +36,19 @@ def _change_data_to_dict(data):
             return data_dictionary
 
 
+log_entry("Listening for incoming connection at {}:{}".format(IP, PORT))
 while True:
     clientsocket, address = s.accept()
-    print("Connection from has been established!")
+    log_entry("Connection from {} has been established!".format(address))
     data = clientsocket.recv(1024).decode("utf-8")
+    log_entry("Successfully decoded data received from {}".format(address))
     data = _change_data_to_dict(data)
     if data:
-        db.enter_data_in_db(data)
+        db_entry = db.enter_data_in_db(data)
+        if not (db_entry[0]):
+            log_entry("ERROR: Failed to save data recvd from {} | DATA: {}".format(address, db_entry[1]))
+            log_entry("{}".format(db_entry[2]))
+        elif db_entry[0]:
+            log_entry("SUCCESS: Saved data recvd from {} | DATA: {}".format(address, db_entry[1]))
+        else:
+            log_entry("Unknown event occurred while saving the data recvd from {}".format(address))
