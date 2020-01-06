d4ed3338fed30889087371f3cff340ecdd8ac9d1
Chandral Thakor <chandral.thakor@gmail.com>
2020-01-06 18:24:09 +0530

    tz

--- chronos/test.py
+++ chronos/test.py
@@ -1,44 +1,13 @@
-import os
-import tarfile
-import multiprocessing
-from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
+from pytz import timezone
 
-last_timestamp = None
+TIMESTAMP_PREFIX_FORMAT = "%Y-%m-%d, %H:%M:%S"
 
+ts = int("1571897721")
+utc = datetime.utcfromtimestamp(ts)
+print(utc)
+print(utc + timedelta(hours=6, minutes=30))
+print(utc.astimezone(timezone("Asia/Rangoon")))
 
-def create_logs_directory():
-    Path(os.getcwd() + '/Logs').mkdir(parents=True, exist_ok=True)
-
-
-def create_backup_directory():
-    Path(os.getcwd() + '/Logs Backup').mkdir(parents=True, exist_ok=True)
-
-
-def make_tarfile(output_filename, source_dir):
-    with tarfile.open(output_filename, "w:gz") as tar:
-        tar.add(source_dir, arcname=os.path.basename(source_dir))
-
-
-def take_backup():
-    pass
-
-
-backup_process = multiprocessing.Process(target=make_tarfile, args=())
-
-
-def log(label, msg):
-    global last_timestamp  # DO NOT remove this reference to the global variable 'last_timestamp' created above
-    current_timestamp = datetime.now()
-
-    if (current_timestamp - last_timestamp).days != 0:
-        backup_process.start()
-
-    file_prefix = current_timestamp.strftime("%Y_%m_%d_")
-    log_file = open("{}server.log".format(file_prefix), "a")  # ENSURE the I/O permission remains 'a'
-    log_timestamp = "{} | ".format(current_timestamp.strftime("%Y-%m-%d, %H:%M:%S"))
-    log_msg = log_timestamp + label + ' | ' + msg
-    log_file.write(log_msg + "\n")
-    log_file.close()
-    last_timestamp = current_timestamp
-    print(log_msg)
+def unix_to_rangoon_tz(unix_stamp):
+    str_to_int = int
