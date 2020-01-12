49f7cb40f04e2b57d43568b8834e9a6cd0873a8d
Chandral Thakor <chandral.thakor@gmail.com>
2020-01-12 11:31:49 +0530

    file io

--- file-io/app.py
+++ file-io/app.py
@@ -1,7 +1,10 @@
 with open("test.txt", "r+") as f:
-    d = f.readlines()
+    file_rewrite = []
+    lines = f.readlines()
+    for line in lines:
+        if 'a' not in line.lower():
+            file_rewrite.append(line)
+    f.truncate(0)
     f.seek(0)
-    for i in d:
-        if 'x' in i:
-            f.write(i)
-    f.truncate()
+    for line in file_rewrite:
+        f.write(line)
