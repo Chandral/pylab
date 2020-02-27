b2cf938604a35c2c46ff128d944dcc4cacc777f6
Chandral Thakor <chandral.thakor@gmail.com>
2020-02-27 10:47:26 +0530

    Added program to ping IPs

--- test/example.py
+++ test/example.py
@@ -1,21 +1,5 @@
-#!/usr/bin/env python
-# input("Example Input: ")
-
-import datetime as dt
-
-a = {"A": 1, "B": "TEST", "C":dt.datetime.now()}
-
-sql_part1 = "INSERT INTO table ("
-sql_part2 = "VALUES ("
-for k, v in a.items():
-    sql_part1 += k + ','
-    if isinstance(v, str):
-        sql_part2 += v + ','
-    else:
-        sql_part2 += str(v) + ','
-
-
-
-sql_part1 += ')'
-sql_part2 += ')'
-print(sql_part1[:-1])
+a = None
+try:
+    a["1"]
+except Exception as e:
+    print(e)
