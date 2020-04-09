8b6497a6336bccddda1cc2e129fba86d68b36f16
chandral <chandral.thakor@gmail.com>
2020-04-09 15:37:36 +0530

    Sentdex socket server: Part 3

--- Socket Programming/Sentdex/client.py
+++ Socket Programming/Sentdex/client.py
@@ -1,11 +1,12 @@
 import socket
+import pickle
 
 HEADER_SIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-
+"""
 while True:
     full_msg = ''
     new_msg = True
@@ -21,3 +22,21 @@ while True:
             break
 
     print(full_msg)
+"""
+
+while True:
+    full_msg = b''
+    new_msg = True
+    while True:
+        msg = s.recv(HEADER_SIZE)
+        if new_msg:
+            msg_len = int(msg[:HEADER_SIZE].decode())
+            print(msg_len)
+            new_msg = False
+            continue
+        full_msg += msg
+        if len(full_msg) == msg_len:
+            print(full_msg)
+            break
+
+    print(pickle.loads(full_msg))
--- Socket Programming/Sentdex/server.py
+++ Socket Programming/Sentdex/server.py
@@ -1,7 +1,12 @@
 import time
+import pickle
 import socket
 
 HEADER_SIZE = 10
+sample_data = {"Hey": 1, "There": 2}
+
+
+"""
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allows to use the same IP/Port on server restart
 s.bind((socket.gethostname(), 1234))
@@ -17,3 +22,15 @@ while True:
         msg = f"The time is {time.time()}"
         msg = f'{len(msg):<{HEADER_SIZE}}' + msg
         clientsocket.send(msg.encode('utf-8'))
+"""
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allows to use the same IP/Port on server restart
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established!")
+    msg = pickle.dumps(sample_data)
+    msg = bytes(f'{len(msg):<{HEADER_SIZE}}', 'utf-8') + msg
+    clientsocket.send(msg)
