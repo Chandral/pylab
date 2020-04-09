3fcf3c3279f0f04556218bd3e934e86b22fc82e7
chandral <chandral.thakor@gmail.com>
2020-04-09 14:38:56 +0530

    Sentdex socket server: Part 2

--- Socket Programming/Sentdex/client.py
+++ Socket Programming/Sentdex/client.py
@@ -1,13 +1,23 @@
 import socket
 
+HEADER_SIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
-full_msg = ''
+
 
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(HEADER_SIZE)
+        if new_msg:
+            msg_len = int(msg[:HEADER_SIZE].decode())
+            print(msg_len)
+            new_msg = False
+            continue
+        full_msg += msg.decode()
+        if len(full_msg) == msg_len:
+            break
 
-print(full_msg)
+    print(full_msg)
--- Socket Programming/Sentdex/server.py
+++ Socket Programming/Sentdex/server.py
@@ -1,5 +1,7 @@
+import time
 import socket
 
+HEADER_SIZE = 10
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allows to use the same IP/Port on server restart
 s.bind((socket.gethostname(), 1234))
@@ -7,5 +9,11 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send("Welcome to the server".encode())
-    clientsocket.close()
+    msg = "Welcome to the server!"
+    msg = f'{len(msg):<{HEADER_SIZE}}' + msg
+    clientsocket.send(msg.encode('utf-8'))
+    while True:
+        time.sleep(3)
+        msg = f"The time is {time.time()}"
+        msg = f'{len(msg):<{HEADER_SIZE}}' + msg
+        clientsocket.send(msg.encode('utf-8'))
